c0894d8d7465a6536d015429ed30d7ab070338c9
kemperset <1612867@senati.pe>
2025-05-28 00:12:09 -0500

    Nombre de la empresa en Dahsboard

--- app/views/default/navigation.php
+++ app/views/default/navigation.php
@@ -3,9 +3,9 @@
         <!-- Logo -->
         <a href="index2.html" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>D</b>RS</span>
+          <span class="logo-mini"><b>B</b>SS</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>D'</b>Rossi</span>
+          <span class="logo-lg"><b>Botica </b>ServiSalud</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
