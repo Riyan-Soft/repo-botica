3e18f907e9e06407ae8a5936e39ec7ff1c3ab466
Rider • RiyanPC <firstgamer303@gmail.com>
2025-05-29 13:01:18 -0500

    fix

--- app/views/default/navigation.php
+++ app/views/default/navigation.php
@@ -3,9 +3,9 @@
         <!-- Logo -->
         <a href="index2.html" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>D</b>RS</span>
+          <span class="logo-mini"><b>SSD</b></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>D'</b>Rossi</span>
+          <span class="logo-lg"><b>Servi</b>Salud</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
@@ -18,55 +18,13 @@
               <!-- Notifications: style can be found in dropdown.less -->
               <li class="dropdown notifications-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-bell-o"></i>
-                  <span class="label label-warning">10</span>
                 </a>
-                <ul class="dropdown-menu">
-                  <li class="header">You have 10 notifications</li>
-                  <li>
-                    <!-- inner menu: contains the actual data -->
-                    <ul class="menu">
-                      <li>
-                        <a href="#">
-                          <i class="fa fa-users text-aqua"></i> 5 new members
-                          joined today
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i class="fa fa-warning text-yellow"></i> Very long
-                          description here that may not fit into the page and
-                          may cause design problems
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i class="fa fa-users text-red"></i> 5 new members
-                          joined
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i class="fa fa-shopping-cart text-green"></i> 25
-                          sales made
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i class="fa fa-user text-red"></i> You changed your
-                          username
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="footer"><a href="#">View all</a></li>
-                </ul>
               </li>
-              <!-- User Account: style can be found in dropdown.less -->
+            <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                   <img
-                    src="html/dist/img/user2-160x160.jpg"
+                    src="html/dist/img/black bulls.jpg"
                     class="user-image"
                     alt="User Image" />
                   <span class="hidden-xs">
@@ -78,23 +36,23 @@
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
-                  <li class="user-header">
+                  <li class="user-header" id="user-header-color">
                     <img
-                      src="html/dist/img/user2-160x160.jpg"
+                      src="html/dist/img/black bulls.jpg"
                       class="img-circle"
                       alt="User Image" />
                     <p>
                       <?php //inicio
-                      echo $nombre_usuario . " " . $apellido_usuario
+                      echo "Bienvenido <br>" . $nombre_usuario . " " . $apellido_usuario
                       //cierre 
-                      ?> - Web Developer
-                      <small>Member since Nov. 2012</small>
+                      ?>
+                      <small class="textitos"> Cerrar Sesión solo por aquí</small>
                     </p>
                   </li>
                   <!-- Menu Footer-->
-                  <li class="user-footer">
+                  <li class="user-footer" id="user-footer-changes">
                     <div class="pull-left">
-                      <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                      <a href="#" class="btn btn-default btn-flat">Click Me</a>
                     </div>
                     <div class="pull-right">
                       <a href="../controllers/cerrar_sesion.php" class="btn btn-default btn-flat"> Cerrar Sesion</a>
@@ -113,7 +71,7 @@
           <!-- Sidebar user panel -->
           <div class="user-panel">
             <div class="pull-left image">
-              <img src="html/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
+              <img src="html/dist/img/black bulls.jpg" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
               <p><?php //inicio
@@ -125,7 +83,7 @@
           </div>
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">MAIN NAVIGATION</li>
+            <li class="header">Navegación principal</li>
             <?php
             // 1 = ADMIN  -- 2 = USUARIO NORMAL
             if ($privilegios_usuario == 1) {
